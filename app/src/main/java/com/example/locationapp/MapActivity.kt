65464beaf41ffba891cbde8b6468deb085c1d021
chan <vsvx13@naver.com>
2023-04-01 13:11:05 +0900

    위치 정보 시스템 가져오기, locationRequest로 5초마다 한 번씩 위치를 자세하게 가져오기, locationCallback으로 받은 위치의  위경도 가져오기, fusedLocationClient는 LocationServices로 주기적으로 업데이트 하는 함수인 requestLocationupdates사용 파라미터에 locationReqeust, locationCallback을 인자로 받고, 메인 루프로 실행, 위치 정보를 사용하기 위해서는 매니페스트에 위치 권한을 가져오고, 위치 권한을 요청하는 코드 작성, 위치 권한은 reqeustLocationPermission 함수와 getCurrentLocation 함수에 사용

--- app/src/main/java/com/example/locationapp/MapActivity.kt
+++ app/src/main/java/com/example/locationapp/MapActivity.kt
@@ -1,10 +1,21 @@
 package com.example.locationapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.example.locationapp.databinding.ActivityMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,8 +26,39 @@ import com.kakao.sdk.common.util.Utility
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
     private lateinit var binding : ActivityMapBinding
     private lateinit var googleMap : GoogleMap
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            //새로 요청된 위치 정보
+            for(location in locationResult.locations){
+
+                //새로 요청된 위치의 위경도
+                Log.e("MapActivity", "onLocationResult : ${location.latitude}, ${location.longitude}")
+
+            }
+        }
+    }
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ){permissions ->
+        when{
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                //fine location 권한이 있다.
+                getCurrentLocation()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                //coarse location 권한이 있다.
+                getCurrentLocation()
+            }
+            else -> {
+                //TODO 설정으로 보내기 or 교육용 팝을 띄워서 다시 권한 요청하기
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +68,50 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        //권한 요청
+        requestLocationPermission()
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun getCurrentLocation() {
+        val locationRequest = LocationRequest
+            .Builder(Priority.PRIORITY_HIGH_ACCURACY, 5 * 1000).build()
+
+        //권한 확인
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+
+            return
+        }
+
+        //권한이 있는 상태
+        fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+    }
+
+    //권한 요청
+    private fun requestLocationPermission(){
+        locationPermissionRequest.launch(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        )
     }
 
     override fun onMapReady(map: GoogleMap) {
